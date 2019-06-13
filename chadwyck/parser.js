da8d7e6edfabc62efc0f845c2afb3992592e84c1
Lisa Hamlett <lhamlet@emory.edu>
2019-06-13 18:07:37 -0400

    Updates Parser for Chadwyck

--- chadwyck/parser.js
+++ chadwyck/parser.js
@@ -43,35 +43,73 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   }
 
   if (result.rtype !== null) {
-    if (/(acta.chadwyck.com|acta.chadwyck.co.uk)/i.test(host)) {
+    if (/(aabd.chadwyck.com|aabd.chadwyck.com.co.uk)/i.test(host)) {
+      // aabd.chadwyck.com
+      result.publication_title = 'African American Biographical Database';
+    } else if (/(acta.chadwyck.com|acta.chadwyck.co.uk)/i.test(host)) {
       // acta.chadwyck.com
       result.publication_title = 'Acta Sanctorum';
-    } else if (/ble.chadwyck.com/i.test(host)) {
-      // ble.chadwyck.com
-      result.publication_title = 'Chadwyck Bibliografia de la Literature Espanola';
-    } else if (/britishperiodicals.chadwyck.co.uk/i.test(host)) {
-      // britishperiodicals.chadwyck.co.uk
-      result.publication_title = 'Chadwyck ProQuest British Periodicals';
-    } else if (/colonial.chadwyck.com/i.test(host)) {
-      // colonial.chadwyck.com
-      result.publication_title = 'Chadwyck Colonial State Papers Collection';
-    } else if (/collections.chadwyck.com/i.test(host)) {
+    } else if (/(bsc.chadwyck.com|bsc.chadwyck.co.uk)/i.test(host)) {
+      // bsc.chadwyck.com
+      result.publication_title = 'Black Studies Center';
+    } else if (/(collections.chadwyck.com|collections.chadwyck.co.uk)/i.test(host)) {
       // collections.chadwyck.com
-      result.publication_title = 'Chadwyck Literature Collections';
-    } else if (/lion.chadwyck.com/i.test(host)) {
-      // lion.chadwyck.com
-      result.publication_title = 'Chadwyck Literature Online';
-    } else if (/pld.chadwyck.co.uk/i.test(host)) {
-      // pld.chadwyck.co.uk
-      result.publication_title = 'Chadwyck Patrologia Latina';
-    } else if (/www.journal.csj.jp/i.test(host)) {
-      // www.journal.csj.jp
-      result.publication_title = 'Chemistry Letters';
+      result.publication_title = 'Chadwyck-Healey Literature Collections';
+    } else if (/(eebo.chadwyck.com|eebo.chadwyck.co.uk)/i.test(host)) {
+      // eebo.chadwyck.com
+      result.publication_title = 'Early English Books Online';
+    } else if (/(gerritsen.chadwyck.com|gerritsen.chadwyck.co.uk)/i.test(host)) {
+      // gerritsen.chadwyck.com
+      result.publication_title = 'The Gerritsen Collection';
+    } else if (/(glc.chadwyck.com|glc.chadwyck.co.uk)/i.test(host)) {
+      // glc.chadwyck.com
+      result.publication_title = 'German Literature Collections';
+    } else if (/(glp.chadwyck.com|glp.chadwyck.co.uk)/i.test(host)) {
+      // glp.chadwyck.com
+      result.publication_title = 'Die Deutsche Lyrik';
+    } else if (/(goethe.chadwyck.com|goethe.chadwyck.co.uk)/i.test(host)) {
+      // goethe.chadwyck.com
+      result.publication_title = 'Goethes Werke';
+    } else if (/(history.chadwyck.com|history.chadwyck.co.uk)/i.test(host)) {
+      // history.chadwyck.co.uk
+      result.publication_title = 'Historical Newspapers';
+    } else if (/(historymakers.chadwyck.com|historymakers.chadwyck.co.uk)/i.test(host)) {
+      // historymakers.chadwyck.com
+      result.publication_title = 'The HistoryMakers';
+    } else if (/(historynews.chadwyck.com|historynews.chadwyck.co.uk)/i.test(host)) {
+      // historynews.chadwyck.com
+      result.publication_title = 'Historical Newspapers';
+    } else if (/(iibp.chadwyck.com|iibp.chadwyck.co.uk)/i.test(host)) {
+      // iibp.chadwyck.com
+      result.publication_title = 'Index to Black Periodicals';
+    } else if (/(johnjohnson.chadwyck.com|johnjohnson.chadwyck.co.uk)/i.test(host)) {
+      // johnjohnson.chadwyck.com
+      result.publication_title = 'The John Johnson Collection';
+    } else if (/(kafka.chadwyck.com|kafka.chadwyck.co.uk)/i.test(host)) {
+      // kafka.chadwyck.com
+      result.publication_title = 'Kafka Werke';
+    } else if (/(klassiker.chadwyck.com|klassiker.chadwyck.co.uk)/i.test(host)) {
+      // klassiker.chadwyck.com
+      result.publication_title = 'Bibliothek deutscher Klassiker';
     } else if (/(luther.chadwyck.com|luther.chadwyck.co.uk)/i.test(host)) {
       // luther.chadwyck.com
       result.publication_title = 'Luthers Werke';
+    } else if (/(nstc.chadwyck.com|nstc.chadwyck.co.uk)/i.test(host)) {
+      // nstc.chadwyck.com
+      result.publication_title = 'Nineteenth-Century Short Title Catalogue';
+    } else if (/(pld.chadwyck.com|pld.chadwyck.co.uk)/i.test(host)) {
+      // pld.chadwyck.co.uk
+      result.publication_title = 'Patrologia Latina';
+    } else if (/(qvj.chadwyck.com|qvj.chadwyck.co.uk)/i.test(host)) {
+      // qvj.chadwyck.com
+      result.publication_title = 'Queen Victoria\'s Journals';
+    } else if (/(teso.chadwyck.com|teso.chadwyck.co.uk)/i.test(host)) {
+      // teso.chadwyck.com
+      result.publication_title = 'Teatro Español del Siglo de Oro';
+    } else if (/(wellesley.chadwyck.com|wellesley.chadwyck.co.uk)/i.test(host)) {
+      // wellesley.chadwyck.co.uk
+      result.publication_title = 'The Wellesley Index to Victorian Periodicals, 1824-1900';
     }
   }
-
   return result;
 });
