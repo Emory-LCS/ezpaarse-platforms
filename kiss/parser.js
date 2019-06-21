8fb3bf0b0014c24bf551669c6a8d2a2ad4a10cd0
clare <grimalkinclare@gmail.com>
2019-06-21 15:09:21 -0400

    adds SEARCH, REF, and TOC to KISS

--- kiss/parser.js
+++ kiss/parser.js
@@ -14,34 +14,48 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  let match;
+  // let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if (/^\/subject\/(subject([0-9]+)|subj-thesis-view)\.asp/i.test(path)) {
+    // http://kiss.kstudy.com:80/subject/subject2.asp?datas=06,26,kiss
+    // http://kiss.kstudy.com:80/subject/subject1.asp?datas=06,31,kiss
+    // http://kiss.kstudy.com:80/subject/subject2.asp
+    // http://kiss.kstudy.com:80/subject/subj-thesis-view.asp?key=8
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
+  } else if (/^\/dataReport\/data2\.asp/i.test(path)) {
+    // http://kiss.kstudy.com:80/dataReport/data2.asp?key=204
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+  } else if (/^\/inc\/aside_view_thesis\.asp/i.test(path)) {
+    // http://kiss.kstudy.com:80/inc/aside_view_thesis.asp?key=3660962,3217329,3656253
+    // http://kiss.kstudy.com:80/inc/aside_view_thesis.asp?key=3660962,3217329,3656253,2851753,404440,404449,3664469,1637421,3581678,3186305,3666921,3547940,3667618,3587276
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+
+  } else if (/^\/publish\/pub-office\.asp/i.test(path)) {
+    // http://kiss.kstudy.com:80/publish/pub-office.asp?queryIn=7113
+    // http://kiss.kstudy.com:80/publish/pub-office.asp?queryIn=7120
+    result.rtype = 'REF';
+    result.mime = 'HTML';
+
+  } else if (/^\/journal\/journal-view\.asp/i.test(path)) {
+    // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=30199&key2=7136
+    // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=30127&key2=7129
+    // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=28637&key2=3002
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+    result.unitid = param.key1 + '/' + param.key2;
+
   }
+
 
   return result;
 });
