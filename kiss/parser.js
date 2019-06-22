15698f9b32c5f1f233208fe6c720636f5be012e6
clare <grimalkinclare@gmail.com>
2019-06-21 20:40:35 -0400

    finished parser for KISS

--- kiss/parser.js
+++ kiss/parser.js
@@ -22,7 +22,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // let match;
 
   if (/^\/subject\/(subject([0-9]+)|subj-thesis-view)\.asp/i.test(path)) {
-    // http://kiss.kstudy.com:80/subject/subject2.asp?datas=06,26,kiss
     // http://kiss.kstudy.com:80/subject/subject1.asp?datas=06,31,kiss
     // http://kiss.kstudy.com:80/subject/subject2.asp
     // http://kiss.kstudy.com:80/subject/subj-thesis-view.asp?key=8
@@ -49,7 +48,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   } else if (/^\/journal\/journal-view\.asp/i.test(path)) {
     // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=30199&key2=7136
     // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=30127&key2=7129
-    // http://kiss.kstudy.com:80/journal/journal-view.asp?key1=28637&key2=3002
     result.rtype = 'TOC';
     result.mime = 'HTML';
     result.unitid = param.key1 + '/' + param.key2;
@@ -57,16 +55,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   } else if (/^\/thesis\/thesis-view\.asp/i.test(path)) {
     // http://kiss.kstudy.com:80/thesis/thesis-view.asp?key=3660962
     // http://kiss.kstudy.com:80/thesis/thesis-view.asp?key=3217329
-    // http://kiss.kstudy.com:80/thesis/thesis-view.asp?key=3656253
     result.rtype = 'ABS';
     result.mime = 'HTML';
     result.unitid = param.key;
 
   } else if (/^\/search\/(download|open_viewer)\.asp/i.test(path)) {
     // http://kiss.kstudy.com:80/search/download.asp?ftproot=http://210.101.116.15/kiss5/download_journal.asp&inst_key=7129&a_imag=2c800098.pdf&isDownLoad=0&publ_key=30127
-    // http://kiss.kstudy.com:80/search/download.asp?ftproot=http://210.101.116.15/kiss5/download_journal.asp&inst_key=7129&a_imag=2c800099.pdf&isDownLoad=0&publ_key=30127
-    // http://kiss.kstudy.com:80/search/download.asp?ftproot=http://210.101.116.15/kiss5/download_journal.asp&inst_key=2321&a_imag=2u301418.pdf&isDownLoad=1&publ_key=30772&down_name=%E2%80%98%EC%8B%A0%EC%8B%9C%EB%8C%80%E2%80%99%EC%99%80%20%EC%A4%91%EA%B5%AD%EC%9D%98%20%EC%97%AD%EC%82%AC%20%EB%8B%A4%EC%8B%9C%20%EC%93%B0%EA%B8%B0%20%EC%9D%BC%EA%B5%AD%EC%82%AC%EC%99%80%20%EC%A7%80%EC%97%AD%EC%82%AC%EC%9D%98%20%EA%B2%BD%EA%B3%84%EC%97%90%EC%84%9C
-    // http://kiss.kstudy.com:80/search/open_viewer.asp?ftproot=http://210.101.116.61/74000018/index.asp&inst_key=5050&a_imag=74000018.pdf&isDownLoad=0&publ_key=25492
     // http://kiss.kstudy.com:80/search/open_viewer.asp?ftproot=http://210.101.116.61/06006260/index.asp&inst_key=8010&a_imag=06006260.pdf&isDownLoad=0&publ_key=25450
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
