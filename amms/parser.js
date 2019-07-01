1710b4f5a808d4e34f732cbd980f61eba1496295
Lisa Hamlett <lhamlet@emory.edu>
2019-07-01 18:53:30 -0400

    Updates Parser for American Mathematical Society

--- amms/parser.js
+++ amms/parser.js
@@ -16,8 +16,7 @@ var Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   var result = {};
   var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // var param  = parsedUrl.query || {};
+  var param  = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
@@ -26,22 +25,80 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   if ((match = /^\/([a-z]+)\/search\/([a-z]+).html$/.exec(path)) !== null) {
     // http://www.ams.org/mathscinet/search/publications.html?
-    //http://www.ams.org/mathscinet/search/journaldoc.html?cn=Theory_and_Decision
-    //http://www.ams.org/mathscinet/search/publdoc.htm
+    // http://www.ams.org/mathscinet/search/journaldoc.html?cn=Theory_and_Decision
+    // http://www.ams.org/mathscinet/search/publdoc.htm
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
     if (/([a-z]+)doc/.test(match[2])) {
       result.rtype    = 'REF';
     }
-    //result.title_id = match[1];
-    //result.unitid   = match[2];
+    if (/author/.test(match[2])) {
+      result.rtype    = 'BIO';
+      result.unitid   = param.mrauthid;
+    }
   } else if ((match = /^\/([a-z]+)\/pdf\/([0-9]+).pdf$/.exec(path)) !== null) {
     // http://www.ams.org/mathscinet/pdf/3477652.pdf?
     result.rtype    = 'REF';
     result.mime     = 'PDF';
     result.unitid   = match[2];
-  }
 
+  } else if (((match = /^\/journals\/([a-z]+)\/([0-9-]+)\/$/.exec(path)) !== null) || ((match = /^\/journals\/([a-z]+)\/([0-9-]+)\/home.html$/.exec(path)) !==null)) {
+    // https://www.ams.org:443/journals/jams/2019-32-02/
+    // https://www.ams.org:443/journals/jams/2011-24-03/home.html
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.unitid   = match[2];
+    result.title_id = match[2];
+    result.publication_title = match[1];
+  } else if ((match = /^\/cgi-bin\/mstrack\/([a-z-_]+)\/([a-z-]+)$/.exec(path)) !== null) {
+    // https://www.ams.org:443/cgi-bin/mstrack/accepted_papers/tran
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+    result.title_id = match[1];
+    result.publication_title = match[2];
+  } else if ((match = /^\/cgi-bin\/mstrack\/([a-z-_]+)?$/.exec(path)) !== null) {
+    // https://www.ams.org:443/cgi-bin/mstrack/accepted_papers?jrnl=jams
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+    result.title_id = match[1];
+    result.publication_title = param.jrnl;
+  } else if ((match = /^\/journals\/([a-z]+)\/([0-9-]+)\/([a-zA-Z0-9-]+)\/$/.exec(path)) !== null) {
+    // https://www.ams.org:443/journals/jams/2019-32-02/S0894-0347-2018-00906-3/
+    result.rtype    = 'ABS';
+    result.mime     = 'HTML';
+    result.unitid   = match[3];
+    result.title_id = match[2];
+    result.publication_title = match[1];
+  } else if ((match = /^\/journals\/([a-z]+)\/([0-9-]+)\/([a-zA-Z0-9-]+).pdf$/.exec(path)) !== null) {
+    // https://www-ams-org:443/journals/notices/201904/201904FullIssue.pdf
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = match[3];
+    result.title_id = match[2];
+    result.publication_title = match[1];
+  } else if ((match = /^\/journals\/([a-z]+)\/([0-9-]+)\/([a-zA-Z0-9-]+)\/([a-zA-Z0-9-]+).pdf$/.exec(path)) !== null) {
+    // https://www.ams.org:443/journals/jams/2019-32-02/S0894-0347-2018-00906-3/S0894-0347-2018-00906-3.pdf
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = match[4];
+    result.title_id = match[2];
+    result.publication_title = match[1];
+  } else if ((match = /^\/([a-z-]+)\/([a-z-]+)\/([a-z0-9-]+)/.exec(path)) !== null) {
+    // http://www.ams.org:80/publicoutreach/feature-column/fcarc-brai
+    // https://www.ams.org:443/publicoutreach/math-imagery/re
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.unitid   = match[3];
+    result.title_id = match[1] + '/' + match[2];
+  } else if ((match = /^\/([a-z-]+)\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([a-z0-9-]+)/.exec(path)) !== null) {
+    // https://blogs.ams.org:443/matheducation/2019/02/14/mathematics-gatekeeper-or-gateway/
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.unitid   = match[5];
+    result.title_id = match[1] + '/' + match[2] + '/' + match[3] + '/' + match[4];
+  }
   return result;
 });
 
