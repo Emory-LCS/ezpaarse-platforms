14ffc7dd8ea7748f13733188151364e9a25b292f
clare <grimalkinclare@gmail.com>
2019-07-18 14:46:15 -0400

    adds ISSUE and AUDIO to jwatch parser

--- jwatch/parser.js
+++ jwatch/parser.js
@@ -39,13 +39,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.title_id = match[1];
     }
 
-  } if ((match = /^\/index\.php\/([0-9a-z-]+)\/([0-9/]{10})\/$/i.exec(path)) !== null) {
-    // https://podcasts.jwatch.org:443/index.php/podcast-226-what-we-need-to-talk-about-when-we-talk-about-health/2019/06/11/
-    result.rtype = 'ABS';
-    result.mime = 'HTML';
-    result.unitid = match[1].slice(0, 11);
-    result.title_id = match[1] + '/' + match[2];
-
   } if ((match = /^\/([a-z]{2})([0-9]+)\/([0-9/]{10})\/([a-z-]+)/i.exec(path)) !== null) {
     // https://www.jwatch.org:443/fw115583/2019/07/08/synthetic-cannabinoids-associated-with-more-comas-and
     // https://www.jwatch.org:443/na47534/2018/10/05/bleeding-synthetic-cannabinoid-additives-coming-ed-near
@@ -70,6 +63,36 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime = 'HTML';
     result.unitid = match[1] + '/' + match[2];
     result.title_id = match[3];
+
+  } if ((match = /^\/content\/([a-z-]+)\/([0-9]{4})\/([0-z.,%-/]+)\.pdf$/i.exec(path)) !== null) {
+    // https://www.jwatch.org:443/content/gastroenterology/2019/January%202019,%20Vol.%2020,%20No.%201,%20pp.%201-8.pdf
+    // https://www.jwatch.org:443/content/infectious-diseases/2019/June%202019,%20Vol.%2022,%20No.%206,%20pp.%2051-60.pdf
+    result.rtype = 'ISSUE';
+    result.mime = 'PDF';
+    result.unitid = match[1] + ':' + decodeURI(match[3]);
+    result.title_id = match[1] + ':' + decodeURI(match[3]);
+
+  } if ((match = /^\/media\/([A-z]+)([0-9]+)\.mp3$/i.exec(path)) !== null) {
+    // https://podcasts.jwatch.org:443/media/JWPodcast226.mp3
+    result.rtype = 'AUDIO';
+    result.mime = 'MISC';
+    result.unitid = match[2];
+    result.title_id = match[1];
+
+  } if ((match = /^\/index\.php\/(podcast-([0-9]+))([a-z-]+)\/([0-9/]{10})\/$/i.exec(path)) !== null) {
+  // https://podcasts.jwatch.org:443/index.php/podcast-226-what-we-need-to-talk-about-when-we-talk-about-health/2019/06/11/
+  // https://podcasts.jwatch.org:443/index.php/podcast-223-what-are-the-implications-of-the-bp-guidelines/2018/08/14/
+    result.rtype = 'AUDIO';
+    result.mime = 'MISC';
+    result.unitid = match[1] + '/' + match[4];
+    result.title_id = match[3].slice(1);
+
+  } if ((match = /^\/editors\/(AU([0-9]+))/i.exec(path)) !== null) {
+    // https://www.jwatch.org:443/editors/AU3279?editor=Daniel%20Kaul,%20MD
+    result.rtype ='BIO';
+    result.mime = 'HTML';
+    result.unitid = match[1];
+    result.title_id = decodeURI(param.editor);
 
   }
   return result;
