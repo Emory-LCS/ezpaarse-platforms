2c62c0331237d6d601a6356dd787d84db634bf91
Lisa Hamlett <lhamlet@emory.edu>
2019-11-19 15:39:05 -0500

    Adds full parser for British History Online

--- british-history/parser.js
+++ british-history/parser.js
@@ -15,12 +15,52 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let path   = parsedUrl.pathname;
   // let param = parsedUrl.query || {};
 
-  //  let match;
+  let match;
 
   if (/^\/$/i.test(path)) {
     // http://www.british-history.ac.uk:80/
     result.rtype    = 'CONNECTION';
     result.mime     = 'MISC';
+
+  } else if (/^\/search/i.test(path)) {
+    // https://www.british-history.ac.uk:443/search?query=cromwell
+    // https://www.british-history.ac.uk:443/search/place/london/subject/administrative-and-legal/result-type/book-section?query=cromwell
+    // https://www.british-history.ac.uk:443/search/series/rushworth-papers
+    result.rtype      = 'SEARCH';
+    result.mime       = 'MISC';
+
+  } else if (((match = /^\/(catalogue|using-bho|no-series)$/i.exec(path)) !== null) || ((match = /^\/(catalogue|using-bho|no-series)\/([a-zA-Z0-9-]+)/i.exec(path)) !== null)) {
+    result.rtype      = 'TOC';
+    result.mime       = 'HTML';
+    if ((match[2] == null) || (match[2] == undefined)) {
+      // https://www.british-history.ac.uk:443/catalogue
+      // https://www.british-history.ac.uk:443/using-bho
+      result.title_id = match[1];
+      result.unitid   = match[1];
+    } else if ((match[2] !== null) || (match[2] !== undefined)) {
+      // https://www.british-history.ac.uk:443/catalogue/south-east
+      // https://www.british-history.ac.uk:443/using-bho/biography-guide
+      // https://www.british-history.ac.uk:443/no-series/vestry-mins-colechurch-1613-72
+      result.title_id = match[1];
+      result.unitid   = match[1] + '/' + match[2];
+    }
+
+  } else if (((match = /^\/([a-zA-Z0-9-/]+)\/vol([0-9]+)$/i.exec(path)) !== null) || ((match = /^\/([a-zA-Z0-9-/]+)\/vol([0-9]+)\/pp([0-9-]+)$/i.exec(path)) !== null)) {
+    if ((match[3] == null) || (match[3] == undefined)) {
+      // https://www.british-history.ac.uk:443/cal-state-papers/venice/vol3
+      // https://www.british-history.ac.uk:443/rushworth-papers/vol2
+      result.rtype      = 'TOC';
+      result.mime       = 'HTML';
+      result.title_id = match[1];
+      result.unitid   = match[1] + '/vol' + match[2];
+    } else if ((match[3] !== null) || (match[3] !== undefined)) {
+      // https://www.british-history.ac.uk:443/cal-state-papers/venice/vol3/pp152-159
+      // https://www.british-history.ac.uk:443/rushworth-papers/vol2/pp319-378
+      result.rtype      = 'BOOK_SECTION';
+      result.mime       = 'HTML';
+      result.title_id = match[1];
+      result.unitid   = match[1] + '/vol' + match[2] + '/pp' + match[3];
+    }
 
   }
 
