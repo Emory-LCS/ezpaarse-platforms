a872374c692bdee5d054839c30da84cd61b0c41c
Lisa Hamlett <lhamlet@emory.edu>
2019-11-25 15:32:36 -0500

    Makes AGCooper and CB987 changes to British History Online

--- british-history/parser.js
+++ british-history/parser.js
@@ -29,7 +29,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype      = 'SEARCH';
     result.mime       = 'MISC';
 
-  } else if (((match = /^\/(catalogue|using-bho|no-series)$/i.exec(path)) !== null) || ((match = /^\/(catalogue|using-bho|no-series)\/([a-zA-Z0-9-]+)/i.exec(path)) !== null)) {
+  } else if (((match = /^\/(catalogue|using-bho|no-series|petitions)$/i.exec(path)) !== null) || ((match = /^\/(catalogue|using-bho|no-series|petitions)\/([a-zA-Z0-9/-]+)/i.exec(path)) !== null)) {
     result.rtype      = 'TOC';
     result.mime       = 'HTML';
     if ((match[2] == null) || (match[2] == undefined)) {
@@ -49,18 +49,25 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     if ((match[3] == null) || (match[3] == undefined)) {
       // https://www.british-history.ac.uk:443/cal-state-papers/venice/vol3
       // https://www.british-history.ac.uk:443/rushworth-papers/vol2
-      result.rtype      = 'TOC';
-      result.mime       = 'HTML';
+      result.rtype    = 'TOC';
+      result.mime     = 'HTML';
       result.title_id = match[1];
       result.unitid   = match[1] + '/vol' + match[2];
     } else if ((match[3] !== null) || (match[3] !== undefined)) {
       // https://www.british-history.ac.uk:443/cal-state-papers/venice/vol3/pp152-159
       // https://www.british-history.ac.uk:443/rushworth-papers/vol2/pp319-378
-      result.rtype      = 'BOOK_SECTION';
-      result.mime       = 'HTML';
+      result.rtype    = 'BOOK_SECTION';
+      result.mime     = 'HTML';
       result.title_id = match[1];
       result.unitid   = match[1] + '/vol' + match[2] + '/pp' + match[3];
     }
+
+  } else if ((match = /^\/([a-zA-Z0-9-]+)\/([a-zA-Z-]+)\/([0-9]+)$/i.exec(path)) !== null) {
+    // https://www.british-history.ac.uk:443/os-1-to-10560/buckinghamshire/001
+    result.rtype    = 'IMAGE';
+    result.mime     = 'MISC';
+    result.title_id = match[1];
+    result.unitid   = match[1] + '/' + match[2] + '/' + match[3];
 
   }
 
