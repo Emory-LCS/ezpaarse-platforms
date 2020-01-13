11536b879cd212c1449ae01712c5099a7a2481e1
clare <grimalkinclare@gmail.com>
2020-01-13 15:19:44 -0500

    first draft parser and test

--- Digitalia/parser.js
+++ Digitalia/parser.js
@@ -14,33 +14,81 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if (/^\/fulltext\/filter/i.test(path)) {
+    //http://www.digitaliapublishing.com:80/fulltext/filter/tipo/0
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  }
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
+  else if (/^\/co|ejournals|s\/([0-9]+)/i.test(path)) {
+    //www.digitaliapublishing.com:80/co/721
+    //www.digitaliapublishing.com:80/ejournals/119/art
+    //www.digitaliapublishing.com:80/s/98/language-and-linguistics
+    //www.digitaliapublishing.com:80/s/100
+    result.rtype = 'SEARCH*';
+    result.mime = 'HTML';
+  }
+
+  else if ((match = /^\/a\/([0-9]+)\/([0-z-.]+)$/i.exec(path)) !=null) {
+    // http://www.digitaliapublishing.com:80/a/34355/12-arquitectos-contemporaneos (book)
+    // http://www.digitaliapublishing.com:80/a/39177/gestion-de-grandes-proyectos-urbanos-en-espacios-metropolizados---los-sistemas-integrados-de-transporte-masivo-en-colombia
+    // http://www.digitaliapublishing.com:80/a/19774/ajoblanco-segunda-etapa-n.-116 (journal)
+    // http://www.digitaliapublishing.com:80/a/3986/secuencias-no.-25 (journal)
+    // http://www.digitaliapublishing.com:80/a/21321/anuario-musical-numero-66
+    // http://www.digitaliapublishing.com:80/a/3762/metaliteratura-y-metaficcion--balance-critico-y-perspectivas-comparadas (journal)
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+    result.unitid = match[1];
+    result.title_id = match[2];
+  }
+
+  else if ((match = /^\/(visor|visorswf|visorepub)\/([0-9]+)$/i.exec(path)) !=null) {
+    // http://www.digitaliapublishing.com:80/visor/398
+    // http://www.digitaliapublishing.com:80/visorswf/3987
+    result.rtype = 'BOOK';
+    if (match[1] === 'visor') {
+      result.mime = 'PDF';
+    } else if (match[1] === 'visorswf') {
+      result.mime = 'HTML';
+    } else if (match[1] === 'visorepub') {
+      result.mime = 'EPUB';
+    }
     result.unitid = match[2];
+    result.title_id = match[2];
+  }
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+  else if (/^\/index.jsp/i.test(path)) {
+    // http://www.digitaliapublishing.com:80/index.jsp?o=501&id=3762&page=101
+    // http://www.digitaliapublishing.com:80/index.jsp?o=501&id=19774&page=2
+    result.rtype = 'BOOK_SECTION';
+    result.mime = 'PDF';
+    result.unitid = param.id + '_' + param.page;
+    result.title_id = param.id;
+  }
+
+  else if ((match = /^\/visorpagehtml\/([0-9]+)_([0-9]+).page$/i.exec(path)) !=null) {
+    //http://www.digitaliapublishing.com:80/visorpagehtml/3762_16.page
+    // http://www.digitaliapublishing.com:80/visorpagehtml/41628_101.page
+    result.rtype = 'BOOK_SECTION';
+    result.mime = 'HTML';
+    result.unitid = match[1] + '_' + match[2];
     result.title_id = match[1];
-    result.unitid   = match[2];
+  }
+
+  else if ((match = /^\/visorreadspeaker\/([0-9]+)$/i.exec(path)) !=null) {
+    // http://www.digitaliapublishing.com:80/visorreadspeaker/39177
+    // http://www.digitaliapublishing.com:80/visorreadspeaker/19774
+    result.rtype = 'AUDIO';
+    result.mime = 'MISC';
+    result.unitid = match[1];
+    result.title_id = match[1];
   }
 
   return result;
