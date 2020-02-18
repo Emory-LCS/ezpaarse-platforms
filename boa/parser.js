1707f63990ba1685fef9d0c1a92769ac7720f3d5
clare <grimalkinclare@gmail.com>
2020-02-18 14:53:27 -0500

    comments out everything but SEARCH and TOC and removes test lines

--- boa/parser.js
+++ boa/parser.js
@@ -38,41 +38,42 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.unitid = match[2] + '/' + match[3];
       result.title_id = match[2] + '/' + match[3];
     }
+// the following conditions are commented out because they produce multiple false hits for any real hit.
+// leaving them in just in case they could be useful in the future
+  // } else if ((match = /^\/boa\/((documents)|(posts\/category\/(articles|collection-releases|news)))\/([0-9]+)\/([0-z-]+)/i.exec(path)) !== null) {
+  //   // https://microform.digital:443/boa/documents/726/the-blackshirt-february-to-june-1933
+  //   // https://microform.digital:443/boa/posts/category/articles/356/dispatches-from-the-coin-wars-a-post-revisionist-appraisal-of-american-strategy-in-the-vietnam-war
+  //   // https://microform.digital:443/boa/posts/category/collection-releases/349/prosecuting-the-holocaust-british-investigations-into-nazi-war-crimes-1944-1949
+  //   result.rtype = 'ARTICLE';
+  //   result.mime = 'HTML';
+  //   result.unitid = match[5] + '/' + match[6];
+  //   result.title_id = match[5] + '/' + match[6];
 
-  } else if ((match = /^\/boa\/((documents)|(posts\/category\/(articles|collection-releases|news)))\/([0-9]+)\/([0-z-]+)/i.exec(path)) !== null) {
-    // https://microform.digital:443/boa/documents/726/the-blackshirt-february-to-june-1933
-    // https://microform.digital:443/boa/posts/category/articles/356/dispatches-from-the-coin-wars-a-post-revisionist-appraisal-of-american-strategy-in-the-vietnam-war
-    // https://microform.digital:443/boa/posts/category/collection-releases/349/prosecuting-the-holocaust-british-investigations-into-nazi-war-crimes-1944-1949
-    result.rtype = 'ARTICLE';
-    result.mime = 'HTML';
-    result.unitid = match[5] + '/' + match[6];
-    result.title_id = match[5] + '/' + match[6];
+  // } else if ((match = /^\/boa\/documents\/([0-z-]+)\/page-url\/([0-9]+)/i.exec(path)) !== null) {
+  //   // https://microform.digital:443/boa/documents/J-BOX1-01/page-url/2?base64=true
+  //   // https://microform.digital:443/boa/documents/125-bla-1933a/page-url/1?base64=true
+  //   // https://microform.digital:443/boa/documents/Bb-01/page-url/1?base64=true
+  //   // https://microform.digital:443/boa/documents/Bb-01/page-url/9?base64=true
+  //   result.rtype = 'IMAGE';
+  //   result.mime = 'PDF';
+  //   result.unitid = match[1] + '/' + match[2];
+  //   result.title_id = match[1] + '/' + match[2];
+  //   result.print_identifier = match[1];
 
-  } else if ((match = /^\/boa\/documents\/([0-z-]+)\/page-url\/([0-9]+)/i.exec(path)) !== null) {
-    // https://microform.digital:443/boa/documents/J-BOX1-01/page-url/2?base64=true
-    // https://microform.digital:443/boa/documents/125-bla-1933a/page-url/1?base64=true
-    // https://microform.digital:443/boa/documents/Bb-01/page-url/1?base64=true
-    // https://microform.digital:443/boa/documents/Bb-01/page-url/9?base64=true
-    result.rtype = 'IMAGE';
-    result.mime = 'PDF';
-    result.unitid = match[1] + '/' + match[2];
-    result.title_id = match[1] + '/' + match[2];
-    result.print_identifier = match[1];
+  // } else if ((match = /^\/boa\/posts\/category\/podcasts\/([0-z-/]+)/i.exec(path)) !== null) {
+  //   // https://microform.digital:443/boa/posts/category/podcasts/360/remembering-the-holocaust
+  //   // https://microform.digital:443/boa/posts/category/podcasts/332/is-space-the-final-frontier-of-inter-state-rivalry
+  //   result.rtype = 'AUDIO';
+  //   result.mime = 'MISC';
+  //   result.unitid = match[1];
+  //   result.title_id = match[1];
 
-  } else if ((match = /^\/boa\/posts\/category\/podcasts\/([0-z-/]+)/i.exec(path)) !== null) {
-    // https://microform.digital:443/boa/posts/category/podcasts/360/remembering-the-holocaust
-    // https://microform.digital:443/boa/posts/category/podcasts/332/is-space-the-final-frontier-of-inter-state-rivalry
-    result.rtype = 'AUDIO';
-    result.mime = 'MISC';
-    result.unitid = match[1];
-    result.title_id = match[1];
-
-  } else if ((match = /^\/boa\/posts\/category\/video\/([0-z-/]+)/i.exec(path)) !== null) {
-    // https://microform.digital:443/boa/posts/category/video/346/battle-of-britain-meps-on-brexit
-    result.rtype = 'VIDEO';
-    result.mime = 'MISC';
-    result.unitid = match[1];
-    result.title_id = match[1];
+  // } else if ((match = /^\/boa\/posts\/category\/video\/([0-z-/]+)/i.exec(path)) !== null) {
+  //   // https://microform.digital:443/boa/posts/category/video/346/battle-of-britain-meps-on-brexit
+  //   result.rtype = 'VIDEO';
+  //   result.mime = 'MISC';
+  //   result.unitid = match[1];
+  //   result.title_id = match[1];
 
   }
 
