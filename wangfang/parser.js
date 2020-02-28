9904dc14acadcaaf319a6291dee55c5fba2c79cc
clare <grimalkinclare@gmail.com>
2020-02-28 11:31:49 -0500

    first draft wanfang parser

--- wangfang/parser.js
+++ wangfang/parser.js
@@ -14,33 +14,42 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if ((match = /^\/details\/detail.do/i.exec(path)) != null) {
+    // http://www.wanfangdata.com.cn:80/details/detail.do?_type=perio&id=MBD2%255C%255CMBD%255C%255CMBD2%255C%255CS1755267209001183h.xml
+    // http://www.wanfangdata.com.cn:80/details/detail.do?_type=degree&id=D01099412
+    result.rtype = 'REF';
+    result.mime = 'HTML';
+    result.unitid = param.id;
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
+  } else if ((match = /^\/search\/searchList.do$/i.exec(path)) || (match = /^\/s\/search\/$/i.exec(path)) != null) {
+    // http://video.wanfangdata.com.cn:80/s/search/?conditionString=CWX12-0-0-0-0-0-0-0-1-0-0/&
+    // http://www.wanfangdata.com.cn:80/search/searchList.do?searchType=tech&showType=&pageSize=&searchWord=oxygen&isTriggerTag=
+    // http://www.wanfangdata.com.cn:80/search/searchList.do?searchType=patent&showType=detail&pageSize=20&searchWord=%E7%94%B3%E8%AF%B7%E4%BA%BA%2F%E4%B8%93%E5%88%A9%E6%9D%83%E4%BA%BA%3A
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
 
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+  } else if ((match = /^\/themeBootPage\/explainAndstatistics\.do$/i.exec(path)) != null) {
+    // http://miner.wanfangdata.com.cn:80/themeBootPage/explainAndstatistics.do?themeWord=United%20states
+    // http://miner.wanfangdata.com.cn:80/themeBootPage/explainAndstatistics.do?themeWord=Female
+    result.rtype = 'DATA';
+    result.mime = 'HTML';
+    result.unitid = param.themeWord;
+    result.title_id = param.themeWord;
+
+  } else if ((match = /^\/v\/play\/([0-9A-Z]+)\.html$/i.exec(path)) != null) {
+    // http://video.wanfangdata.com.cn:80/v/play/SI160419149.html
+    // http://video.wanfangdata.com.cn:80/v/play/SL160727690.html
+    // http://video.wanfangdata.com.cn:80/v/play/SD140110274.html
+    result.rtype = 'VIDEO';
+    result.mime = 'MISC';
+    result.unitid = match[1];
   }
 
   return result;
