64e2c1be043a88cd34007df1ee940a4a2a13ce3b
Lisa Hamlett <lhamlet@emory.edu>
2020-08-04 18:30:22 -0400

    Adds parser for Frontiers in Bioscience

---
+++ bioscience/parser.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Frontiers in Bioscience
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let param = parsedUrl.query || {};
+
+  let match;
+
+  if (/^\/search-article\/index.php$/i.test(path)) {
+    // https://www.bioscience.org:443/search-article/index.php
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+
+  } else if ((match = /^\/(open-access|fast-track|express-open-access)$/i.exec(path)) !== null) {
+    // https://www.bioscience.org:443/open-access
+    // https://www.bioscience.org:443/fast-track
+    // https://www-bioscience-org:443/express-open-access
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+
+  } else if ((match = /^\/(metrics|year-metrics)$/i.exec(path)) !== null) {
+    // https://www.bioscience.org:443/metrics?id=3616
+    // https://www.bioscience.org:443/year-metrics?type=fasttrack
+    result.rtype    = 'DATASET';
+    result.mime     = 'MISC';
+    if (match[1] == 'metrics') {
+      result.unitid = match[1] + '/' + param.id;
+    } else if (match[1] == 'year-metrics') {
+      result.unitid = match[1] + '/' + param.type;
+    }
+
+  } else if (/^\/fbs\/getfile.php$/i.test(path)) {
+    // https://www.bioscience.org:443/fbs/getfile.php?FileName=/2002/v7/d/storz/storz.pdf
+    // https://www.bioscience.org:443/fbs/getfile.php?FileName=/2009/v14/af/3585/3585.pdf
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = param.FileName;
+
+  } else if ((match = /^\/([a-zA-Z0-9_/-]+)\/(list|fulltext|address).htm$/i.exec(path)) !== null) {
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+    if (match[2] == 'list') {
+      // https://www.bioscience.org:443/2002/v7/d/storz/list.htm
+      // http://www.bioscience.org:80/2009/v14/af/3585/list.htm
+      result.rtype = 'ABS';
+    } else if (match[2] == 'fulltext') {
+      // https://www.bioscience.org:443/2002/v7/d/storz/fulltext.htm
+      // http://www.bioscience.org:80/2009/v14/af/3585/fulltext.htm
+      result.rtype = 'ARTICLE';
+    } else if (match[2] == 'address') {
+      // https://www.bioscience.org:443/1998/v3/d/palmada/address.htm
+      // https://www.bioscience.org:443/1998/v3/a/xiao/address.htm
+      result.rtype = 'BIO';
+    }
+
+  }
+
+  return result;
+});
